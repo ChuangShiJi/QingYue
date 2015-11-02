5cbc039ee0f191a1e104a537e3189361323c7ab1
qiangzhen <978823884@qq.com>
2015-11-02 23:10:00 +0800

    开始HomePageFragment界面

--- app/src/main/java/com/chsj/qingyue/MainActivity.java
+++ app/src/main/java/com/chsj/qingyue/MainActivity.java
@@ -1,10 +1,7 @@
 package com.chsj.qingyue;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
 
 
     }
