54aba75627cbc08741496c842a1923f45173ecb3
zQiang <229457269@qq.com>
2015-11-03 09:03:08 +0800

    tijiao

--- app/src/main/java/com/chsj/qingyue/Constants.java
+++ app/src/main/java/com/chsj/qingyue/Constants.java
@@ -10,6 +10,6 @@ public class Constants {
 
     public static final String SP_NAME="app";
     //    欢迎页显示的版本，根据版本判断是否显示
-    public static final String SP_KEY_WELCOME_SHOW_VER="wsv";
+    public static final String SP_KEY_IS_AGREE = "agree";
 
 }
--- app/src/main/java/com/chsj/qingyue/SplashActivity.java
+++ app/src/main/java/com/chsj/qingyue/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.chsj.qingyue;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,40 +11,65 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 /**
  * 活动扉页
  */
-public class SplashActivity extends AppCompatActivity implements  Runnable{
+public class SplashActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private CheckBox checkBox;
+    private int anInt;
+    private SharedPreferences sp;
+
+    private RelativeLayout relative;
+
+    private TextView txt_agree,txt_exit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
 
-        Thread thread = new Thread(this);
+        checkBox = (CheckBox) findViewById(R.id.splash_chexkbox);
+        relative = (RelativeLayout) findViewById(R.id.relative_window);
 
-        thread.start();
+        //检查sp中数据：
+        sp = getSharedPreferences(Constants.SP_NAME, MODE_PRIVATE);
+        anInt = sp.getInt(Constants.SP_KEY_IS_AGREE, 0);
+
+        txt_agree = (TextView) findViewById(R.id.splash_agree);
+        txt_exit = (TextView) findViewById(R.id.splash_exit);
+
+        txt_agree.setOnClickListener(this);
+        txt_exit.setOnClickListener(this);
+
+        isShow();//是否显示   弹出框
 
     }
 
+    @SuppressLint("NewApi")
+    private void isShow() {
+        if (anInt == 0) { //设置  不显示
 
-    /**
-     * 实现Run方法：
-     */
-    @Override
-    public void run() {
+            relative.setVisibility(View.INVISIBLE);
 
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            toNewActivity();
         }
+    }
 
+    private void toNewActivity() {
         /**
          * 开启新的Activity
          */
@@ -55,7 +81,32 @@ public class SplashActivity extends AppCompatActivity implements  Runnable{
         startActivity(intent);
 
         finish();
-
     }
 
+    @Override
+    public void onClick(View v) {
+
+        switch(v.getId()){
+
+            case R.id.splash_agree:
+
+                if (checkBox.isChecked()){
+
+                    SharedPreferences.Editor edit = sp.edit();
+                    edit.putInt(Constants.SP_KEY_IS_AGREE,0);//添加选中
+                    edit.commit();
+
+                }
+
+                toNewActivity();
+
+                break;
+
+            case R.id.splash_exit:
+
+                finish();
+
+                break;
+        }
+    }
 }
